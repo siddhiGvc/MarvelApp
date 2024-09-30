f5508bf262b265cf9239eb75712b0ee3e37753e4
siddhiGvc <siddhi@gvc.in>
2024-09-30 11:04:44 +0530

    vibrator setup page complated

--- app/(tabs)/vibSetup.tsx
+++ app/(tabs)/vibSetup.tsx
@@ -44,7 +44,7 @@ export default function VibSetupScreen() {
             <Text style={styles.menuText}>BULK</Text>
             </View>
             <View style={styles.menu2}>
-            <Text style={styles.menuText}>MADE</Text>
+            <Text style={styles.menuText}>MED</Text>
             </View>
             <View style={styles.menu3}>
             <Text style={styles.menuText}>FINE</Text>
@@ -53,10 +53,178 @@ export default function VibSetupScreen() {
             <Text style={styles.menuText}>S.FINE</Text>
             </View>
          </View>
+         <View style={styles.subContainer2}>
+            <View style={styles.head}>
+            <Text style={styles.menuText}>H1</Text>
+            </View>
+            <View style={styles.row1}>
+              <Text style={styles.menuText}>FRONT</Text>
+              <Text style={styles.menuText}>REAR</Text>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} autoFocus keyboardType="numeric"  />
+                </View>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+           
+         </View>
+         <View style={styles.subContainer2}>
+         <View style={styles.head}>
+            <Text style={styles.menuText}>H2</Text>
+            </View>
+            <View style={styles.row1}>
+              <Text style={styles.menuText}>FRONT</Text>
+              <Text style={styles.menuText}>REAR</Text>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+         </View>
+         <View style={styles.subContainer2}>
+             <View style={styles.head}>
+            <Text style={styles.menuText}>H3</Text>
+            </View>
+            <View style={styles.row1}>
+              <Text style={styles.menuText}>FRONT</Text>
+              <Text style={styles.menuText}>REAR</Text>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+         </View>
+         <View style={styles.subContainer2}>
+             <View style={styles.head}>
+            <Text style={styles.menuText}>H4</Text>
+            </View>
+            <View style={styles.row1}>
+              <Text style={styles.menuText}>FRONT</Text>
+              <Text style={styles.menuText}>REAR</Text>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input1}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input2}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input3}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+            <View style={styles.row1}>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+                <View style={styles.input4}>
+                <TextInput style={styles.textInput} keyboardType="numeric"  />
+                </View>
+            </View>
+         </View>
           
     </View>
-       
-   
+    <View style={styles.subContainer3}>
+        <TouchableOpacity style={styles.button} onPress={() => router.push('/menu')}>
+          <Text style={styles.buttonText}>SAVE DATA & EXIT</Text>
+        </TouchableOpacity>
+        </View>
     </LinearGradient>
    
   );
@@ -73,11 +241,17 @@ const styles = StyleSheet.create({
     
   },
   row: {
-    width:windowWidth/1.3,
+    width:'100%',
     flexDirection: 'row',
-    flexWrap: 'wrap',
     justifyContent:'space-between',
     alignItems:'flex-start'
+  },
+  row1: {
+    width:'100%',
+    height:windowHeight/8,
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    alignItems:'center',
   },
   header: {
     //  flex: 1,
@@ -88,64 +262,145 @@ const styles = StyleSheet.create({
   title:{
     fontSize: 26,
     fontWeight: 'bold',
-    marginBottom: 12,
+    marginBottom: 8,
     color:'yellow'
   },
   subContainer1:{
-    width:windowWidth/5,
+    width:windowWidth/7,
     height:windowHeight/1.42,
     flexDirection:'column',
     justifyContent:'flex-end',
     alignItems:'center',
     
   },
+  subContainer2:{
+    width:windowWidth/4.8,
+    height:windowHeight/1.40,
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:2,
+    borderColor:'white',
+    borderRadius:20
+    
+  },
+  subContainer3:{
+    width:'100%',
+    height:windowHeight/5.5,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+    
+  },
   menu1:{
     
     backgroundColor:'green',
-    width:windowWidth/6,
+    width:windowWidth/8,
     height:windowHeight/10,
     textAlign:'center',
     justifyContent:'center',
     alignItems:'center',
     borderRadius:10,
-    marginVertical:'2%'
+    marginVertical:'4.2%'
   },
   menu2:{
     backgroundColor:'orange',
-    width:windowWidth/6,
+    width:windowWidth/8,
     height:windowHeight/10,
     textAlign:'center',
     justifyContent:'center',
     alignItems:'center',
     borderRadius:10,
-    marginVertical:'2%'
+    marginVertical:'4.2%'
   },
   menu3:{
     backgroundColor:'brown',
-    width:windowWidth/6,
+    width:windowWidth/8,
     height:windowHeight/10,
     textAlign:'center',
     justifyContent:'center',
     alignItems:'center',
     borderRadius:10,
-    marginVertical:'2%'
+    marginVertical:'4.2%'
   },
   menu4:{
     backgroundColor:'red',
-    width:windowWidth/6,
+    width:windowWidth/8,
     height:windowHeight/10,
     textAlign:'center',
     justifyContent:'center',
     alignItems:'center',
     borderRadius:10,
-    marginVertical:'2%'
+    marginVertical:'4.2%'
   },
  menuText:{
     color:'white',
     fontSize:16,
     fontWeight:'500'
- }
- 
+ },
+ button: {
+    height: windowHeight/10.4, // Set button height
+    width: windowWidth/4, // Ensure the button takes the full width of the container
+    backgroundColor: '#007BFF', // Set your button color
+    borderRadius: 10, // Rounded corners
+    justifyContent: 'center', // Center the text vertically
+    alignItems: 'center', // Center the text horizontally
+    marginBottom: 10, // Spacing between buttons
+  },
+  buttonText: {
+    color: '#FFFFFF', // White text color
+    fontSize: 16, // Adjust font size as needed
+  },
+  head:{
+    width:windowWidth/12,
+    backgroundColor:'purple',
+    height:windowHeight/15,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10
+    
+  },
+  input1:{
+    backgroundColor:'green',
+    width:windowWidth/10.2,
+    height:windowHeight/10,
+    textAlign:'center',
+    justifyContent:'space-around',
+    borderRadius:10,
+    color:'white'
+
+  },
+  input2:{
+    backgroundColor:'orange',
+    width:windowWidth/10.2,
+    height:windowHeight/10,
+    textAlign:'center',
+    justifyContent:'space-around',
+    borderRadius:10,
+  },
+  input3:{
+    backgroundColor:'brown',
+    width:windowWidth/10.2,
+    height:windowHeight/10,
+    textAlign:'center',
+    justifyContent:'space-around',
+    borderRadius:10,
+
+  },
+  input4:{
+    backgroundColor:'red',
+    width:windowWidth/10.2,
+    height:windowHeight/10,
+    textAlign:'center',
+    justifyContent:'space-around',
+    borderRadius:10,
+
+  },
+  textInput:{
+    color:'white',
+    textAlign:'center'
+  },
+
 
  
 });
